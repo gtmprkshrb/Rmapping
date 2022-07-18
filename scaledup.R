ab9b812daa4e4c8d9d3bdd44fbefc0756f17277d
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-07-18 18:26:48 +0530

    cleanups

--- scaledup.R
+++ scaledup.R
@@ -14,21 +14,26 @@ con <- dbConnect(
     dataset = "917302307943",
     billing = "tides-saas-309509"
 )
-sql <- "SELECT *  FROM `tides-saas-309509.917302307943.Rscaledup`"
-ds <- bq_dataset("tides-saas-309509", "Rscaledup")
+sql <- "SELECT *  FROM `tides-saas-309509.917302307943.cleanscale`"
+ds <- bq_dataset("tides-saas-309509", "cleanscale")
 tb <- bq_dataset_query(ds,
     query = sql,
     billing = "tides-saas-309509"
 )
 bqdata <- bq_table_download(tb)
-# List of distinct Taluka Names
-KGISTalukName <- bqdata %>%
-    select(KGISTalukName) %>%
+# List of distinct State Names
+State <- bqdata %>%
+    select(State) %>%
     distinct()
 
-# List of distinct Village Names
-KGISVillageName <- bqdata %>%
-    select(KGISVillageName) %>%
+# List of distinct District Names
+District <- bqdata %>%
+    select(District) %>%
+    distinct()
+
+# List of distinct Category Names
+Category <- bqdata %>%
+    select(Category) %>%
     distinct()
 
 ui <- bootstrapPage(
@@ -38,17 +43,25 @@ ui <- bootstrapPage(
         style = "background: #dddddd; padding: 10px",
         top = 10, right = 10, draggable = TRUE,
         selectInput(
-            "KGISVillageName", "Select the Village Name:",
+            "District", "Select the District Name:",
             # Appending ALL to have a option to load all locations
-            append("All", as.list(KGISVillageName$KGISVillageName), ),
+            append("All", as.list(District$District), ),
             # selecting ALL as default option
             selected = "All",
             multiple = TRUE
         ),
         selectInput(
-            "KGISTalukName", "Select the Taluk Name:",
+            "State", "Select the State Name:",
             # Appending ALL to have a option to load all locations
-            append("All", as.list(KGISTalukName$KGISTalukName), ),
+            append("All", as.list(State$State), ),
+            # selecting ALL as default option
+            selected = "All",
+            multiple = TRUE
+        ),
+        selectInput(
+            "Category", "Select the Category Name:",
+            # Appending ALL to have a option to load all locations
+            append("All", as.list(Category$Category), ),
             # selecting ALL as default option
             selected = "All",
             multiple = TRUE
@@ -78,17 +91,24 @@ server <- function(input, output, session) {
     output$mymap <- renderLeaflet({
         filtered_data <- bqdata %>%
             dplyr::filter(
-                if ("All" %in% input$KGISVillageName) {
-                    KGISVillageName != ""
+                if ("All" %in% input$District) {
+                    District != ""
                 } else {
-                    KGISVillageName %in% input$KGISVillageName
+                    District %in% input$District
                 }
             ) %>%
             dplyr::filter(
-                if ("All" %in% input$KGISTalukName) {
-                    KGISTalukName != ""
+                if ("All" %in% input$State) {
+                    State != ""
                 } else {
-                    KGISTalukName %in% input$KGISTalukName
+                    State %in% input$State
+                }
+            ) %>%
+            dplyr::filter(
+                if ("All" %in% input$Category) {
+                    Category != ""
+                } else {
+                    Category %in% input$Category
                 }
             )
         leaflet(filtered_data) %>%
@@ -107,27 +127,34 @@ server <- function(input, output, session) {
                 lat = ~Latitude, lng = ~Longitude,
                 icon = ~ logos[Category],
                 popup = paste0(
+                    "<p> <b>Heading: </b>", filtered_data$Heading, "</p>",
+                    "<p> <b>Description: </b>",
+                    filtered_data$Description,
+                    "</p>",
+                    "<p> <b>State Name: </b>",
+                    filtered_data$State,
+                    "</p>",
                     "<p> <b>District Name: </b>",
-                    filtered_data$KGISDistrictName,
+                    filtered_data$District,
+                    "</p>",
+                    "<p> <b>Address: </b>",
+                    filtered_data$Address,
+                    "</p>",
+                    "<p> <b>Pincode: </b>",
+                    filtered_data$Pincode,
                     "</p>",
                     "<p> <b>Village Name: </b>",
-                    filtered_data$KGISVillageName,
+                    filtered_data$VillageName,
                     "</p>",
-                    "<p> <b>Anganawadi Name: </b>",
-                    filtered_data$AnganawadiName,
-                    "</p>",
-                    "<p> <b>Village Code: </b>",
-                    filtered_data$KGISVillageCode,
-                    "</p>",
-                    "<p> <b>Type: </b>", filtered_data$Type, "</p>",
-                    "<p> <b>Pincode: </b>", filtered_data$Pincode, "</p>",
+                    "<p> <b>Village ID: </b>", filtered_data$VillageID, "</p>",
+                    "<p> <b>WardName: </b>", filtered_data$WardName, "</p>",
                     "<p> <b>Longitude: </b>", filtered_data$Longitude, "</p>",
                     "<p> <b>Latitude: </b>", filtered_data$Latitude, "</p>",
-                    "<p> <b>Anganawadi Worker Name: </b>",
-                    filtered_data$AWWorkerName,
+                    "<p> <b>Ward Number: </b>",
+                    filtered_data$WardNumber,
                     "</p>",
-                    "<p> <b>Anganawadi Worker Phone: </b>",
-                    filtered_data$AWWorkerPhone,
+                    "<p> <b>Taluk Name: </b>",
+                    filtered_data$TalukName,
                     "</p>"
                 ),
                 clusterOptions = markerClusterOptions()
