541fc15ff677c977bccbba03516127d78eab795f
siddhant <ssiddhant3030@gmail.com>
2022-07-29 16:29:19 +0530

    latest code

--- new_change.R
+++ new_change.R
@@ -13,12 +13,16 @@ library(magrittr)
 library(ggmap)
 library(raster)
 library(mapview)
-
+library(mapboxapi)
 
 
 key <- "my_key"
 set_key(key = key)
 register_google(key = key)
+
+my_token <- "my_key"
+
+mapboxapi::mb_access_token(my_token, install = FALSE)
 
 con <- dbConnect(
   bigrquery::bigquery(),
@@ -47,6 +51,61 @@ Category <- bqdata %>%
   dplyr::select(Category) %>%
   distinct()
 
+ui_front <- fluidPage(
+  fluidRow(
+    fluidRow(
+      column(8,
+             leafletOutput("layer_data", height = 700, width = "100%")
+      ),
+      column(4, 
+             selectInput(
+               "District", "Select the District Name:",
+               # Appending ALL to have a option to load all locations
+               append("All", as.list(District$District), ),
+               # selecting ALL as default option
+               selected = "All",
+               multiple = TRUE
+             ),
+             selectInput(
+               "State", "Select the State Name:",
+               # Appending ALL to have a option to load all locations
+               append("All", as.list(State$State), ),
+               # selecting ALL as default option
+               selected = "All",
+               multiple = TRUE
+             ),
+             selectInput(
+               "Category", "Select the Category Name:",
+               # Appending ALL to have a option to load all locations
+               append("All", as.list(Category$Category), ),
+               # selecting ALL as default option
+               selected = "All",
+               multiple = TRUE
+             )
+      )
+    )
+  )
+)
+
+
+
+logos <- awesomeIconList(
+  "Pothole" = makeAwesomeIcon(
+    icon = "road",
+    markerColor = "white",
+    library = "fa"
+  ),
+  "Garbage Collection" = makeAwesomeIcon(
+    icon = "trash",
+    markerColor = "green",
+    library = "fa"
+  ),
+  "Air Quality" = makeAwesomeIcon(
+    icon = "cloud",
+    markerColor = "blue",
+    library = "fa"
+  )
+)
 
 geosearch1 <- basicPage(
   HTML(paste0(" <script> 
@@ -84,126 +143,26 @@ geosearch1 <- basicPage(
                 Shiny.onInputChange('jsValueCoords', coords);});}
                 </script> 
                 <script src='https://maps.googleapis.com/maps/api/js?key=", key,"&libraries=places&callback=initAutocomplete' async defer></script>"))
-  ,leafletOutput('tabContentUI', height = 700, width = "100%")
 )
 
-one <- bootstrapPage(
-  absolutePanel(
-    style = "background: #dddddd; padding: 10px",
-    leafletOutput("mymap", height = 400, width = "210%"),
-    selectInput(
-      "District", "Select the District Name:",
-      # Appending ALL to have a option to load all locations
-      append("All", as.list(District$District), ),
-      # selecting ALL as default option
-      selected = "All",
-      multiple = TRUE
-    ),
-    selectInput(
-      "State", "Select the State Name:",
-      # Appending ALL to have a option to load all locations
-      append("All", as.list(State$State), ),
-      # selecting ALL as default option
-      selected = "All",
-      multiple = TRUE
-    ),
-    selectInput(
-      "Category", "Select the Category Name:",
-      # Appending ALL to have a option to load all locations
-      append("All", as.list(Category$Category), ),
-      # selecting ALL as default option
-      selected = "All",
-      multiple = TRUE
-    )
-  )
-)
-
-two <- fluidPage(
-  fluidRow(
-    column(4, 
-           selectInput(
-             "District", "Select the District Name:",
-             # Appending ALL to have a option to load all locations
-             append("All", as.list(District$District), ),
-             # selecting ALL as default option
-             selected = "All",
-             multiple = TRUE
-           ),
-           selectInput(
-             "State", "Select the State Name:",
-             # Appending ALL to have a option to load all locations
-             append("All", as.list(State$State), ),
-             # selecting ALL as default option
-             selected = "All",
-             multiple = TRUE
-           ),
-           selectInput(
-             "Category", "Select the Category Name:",
-             # Appending ALL to have a option to load all locations
-             append("All", as.list(Category$Category), ),
-             # selecting ALL as default option
-             selected = "All",
-             multiple = TRUE
-           )
-      )
-   ),
-  fluidRow(
-    column(9,
-           leafletOutput("layer_data", height = 400, width = "100%")
-           )
-  )
-)
-
-
-logos <- awesomeIconList(
-  "Pothole" = makeAwesomeIcon(
-    icon = "road",
-    markerColor = "white",
-    library = "fa"
-  ),
-  "Garbage Collection" = makeAwesomeIcon(
-    icon = "trash",
-    markerColor = "green",
-    library = "fa"
-  ),
-  "Air Quality" = makeAwesomeIcon(
-    icon = "cloud",
-    markerColor = "blue",
-    library = "fa"
-  )
-)
 
 ui <- dashboardPage(
+  skin = c("green"),
   dashboardHeader(title = "GeoLocation"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem(
-        "Maps",
-        tabName = "maps",
-        icon = icon("globe"),
-        menuSubItem("layer", tabName = "Layer", icon = icon("map"))
-      )
-    )
-  ),
+  dashboardSidebar(disable = TRUE),
   dashboardBody(
-    tabItems(
-      tabItem(
-        tabName = "geoSearch",
-        geosearch1
-      ),
-      tabItem(
-        tabName = "Layer",
-        two
-      )
+    tabItem(
+      tabName = "Layer",
+      ui_front,
+      geosearch1
     )
   )
 )
+
 
 
 
 server <- function(input, output) {
-  
-  
   
   output$layer_data <- renderLeaflet({
     filtered_data <- bqdata %>%
@@ -231,10 +190,13 @@ server <- function(input, output) {
     
     IND@data <- filtered_data
     
-    leaflet(IND) %>%  addTiles(group = "OpenStreetMap") %>% setView(78.9629, 20.5937, zoom = 4) %>%
+    leaflet(IND) %>%  addMapboxTiles(username = "mapbox", style_id = "streets-v11", group = "mapbox") %>%
+      addMapboxTiles(username = "mapbox", style_id = "outdoors-v11", group = "outdoors") %>%
+      addMapboxTiles(username = "mapbox", style_id = "light-v10", group = "light") %>%
+      addMapboxTiles(username = "mapbox", style_id = "dark-v10", group = "dark") %>%
+      addMapboxTiles(username = "mapbox", style_id = "satellite-v9", group = "satellite") %>%
+      setView(78.9629, 20.5937, zoom = 4) %>%
       
-      addProviderTiles(providers$Esri.WorldStreetMap, options = tileOptions(minZoom=0, maxZoom=13), group = "Esri.WorldStreetMap") %>%
-      addProviderTiles(providers$Esri.WorldImagery, options=tileOptions(minZoom=0, maxZoom=13), group = "Esri.WorldImagery") %>%
       addPolygons(
         weight = 1,
         group = "geo_boundraies",
@@ -341,105 +303,12 @@ server <- function(input, output) {
       
       
       addLayersControl(
-        baseGroups = c("OpenStreetMap", "Esri.WorldStreetMap", "Esri.WorldImagery"),
+        position = "bottomright",
+        baseGroups = c("mapbox", "outdoors", "light", "dark", "satellite"),
         overlayGroups = c("Clustering", "HeatMap", "geo_boundraies", "Markers"),
-        options = layersControlOptions(collapsed=TRUE, )
+        options = layersControlOptions(collapsed=TRUE)
       )
     
-  })
-  
-  output$mymap <- renderLeaflet({
-    filtered_data <- bqdata %>%
-      dplyr::filter(
-        if ("All" %in% input$District) {
-          District != ""
-        } else {
-          District %in% input$District
-        }
-      ) %>%
-      dplyr::filter(
-        if ("All" %in% input$State) {
-          State != ""
-        } else {
-          State %in% input$State
-        }
-      ) %>%
-      dplyr::filter(
-        if ("All" %in% input$Category) {
-          Category != ""
-        } else {
-          Category %in% input$Category
-        }
-      )
-    
-    IND@data <- filtered_data
-    leaflet(IND) %>%
-      # Setting default view to India
-      setView(78.9629, 20.5937, zoom = 5) %>%
-      addProviderTiles(providers$Esri.WorldShadedRelief) %>%
-      addPolygons(
-        weight = 1,
-        stroke = TRUE,
-        color = "transparent",
-        fillOpacity = 0.7,
-        dashArray = "3",
-        highlight = highlightOptions(
-          weight = 2,
-          dashArray = "",
-          color = "red",
-          bringToFront = TRUE
-        )
-      ) %>%
-      addTiles() %>%
-      addHeatmap(
-        lng = ~Longitude,
-        lat = ~Latitude,
-        intensity = 20,
-        max = 100,
-        radius = 20,
-        blur = 10
-      ) %>%
-      addAwesomeMarkers(
-        lat = ~Latitude, lng = ~Longitude,
-        icon = ~ logos[Category],
-        popup = paste0(
-          "<p> <b>Heading: </b>", IND@data$Heading, "</p>",
-          "<img src = ", IND@data$Image,
-          ' width="100%"  height="100"', ">",
-          "<p> <b>Category: </b>",
-          IND@data$Category,
-          "</p>",
-          "<p> <b>Description: </b>",
-          IND@data$Description,
-          "</p>",
-          "<p> <b>State Name: </b>",
-          IND@data$State,
-          "</p>",
-          "<p> <b>District Name: </b>",
-          IND@data$District,
-          "</p>",
-          "<p> <b>Address: </b>",
-          IND@data$Address,
-          "</p>",
-          "<p> <b>Pincode: </b>",
-          IND@data$Pincode,
-          "</p>",
-          "<p> <b>Village Name: </b>",
-          IND@data$VillageName,
-          "</p>",
-          "<p> <b>Village ID: </b>", IND@data$VillageID, "</p>",
-          "<p> <b>WardName: </b>", IND@data$WardName, "</p>",
-          "<p> <b>Longitude: </b>", IND@data$Longitude, "</p>",
-          "<p> <b>Latitude: </b>", IND@data$Latitude, "</p>",
-          "<p> <b>Ward Number: </b>",
-          IND@data$WardNumber,
-          "</p>",
-          "<p> <b>Taluk Name: </b>",
-          IND@data$TalukName,
-          "</p>"
-        ),
-        clusterOptions = markerClusterOptions()
-      )
   })
 }
 
