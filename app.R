fdd916c2cf9611b6059af82c664c1ed5c5adfe03
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-08-16 15:57:46 +0530

    added comments

--- app.R
+++ app.R
@@ -1,4 +1,4 @@
-#install.packages('raster', repos='https://rspatial.r-universe.dev')
+# install.packages('raster', repos='https://rspatial.r-universe.dev')
 
 library(RPostgres)
 library(maps)
@@ -28,15 +28,21 @@ my_token <- Sys.getenv("MAPBOX_TOKEN")
 
 mapboxapi::mb_access_token(my_token, install = TRUE, overwrite = TRUE)
 
-db <- '_95263123ff933d46'
-host_db <- '127.0.0.1'
-db_port <- '5432'
-db_user <- 'postgres'  
-db_password <- '7kGP6f9qjq4jnGlf'
-con <- dbConnect(RPostgres::Postgres(), dbname = db, host=host_db, port=db_port, user=db_user, password=db_password)
+# database name
+db <- "_95263123ff933d46"
+# database host
+host_db <- "127.0.0.1"
+# database port no.
+db_port <- "5432"
+# database user name
+db_user <- "postgres"
+# database password
+db_password <- "7kGP6f9qjq4jnGlf"
+
+con <- dbConnect(RPostgres::Postgres(), dbname = db, host = host_db, port = db_port, user = db_user, password = db_password)
 
 
-bqdata <- dbGetQuery(con, 'SELECT * FROM cleanscale')
+bqdata <- dbGetQuery(con, "SELECT * FROM cleanscale")
 State <- bqdata %>%
   dplyr::select(State) %>%
   distinct()
@@ -54,34 +60,36 @@ Category <- bqdata %>%
 ui_front <- fluidPage(
   fluidRow(
     fluidRow(
-      column(6,
-             leafletOutput("layer_data", height = 500, width = "100%")
+      column(
+        6,
+        leafletOutput("layer_data", height = 500, width = "100%")
       ),
-      column(4, 
-             selectInput(
-               "District", "Select the District Name:",
-               # Appending ALL to have a option to load all locations
-               append("All", as.list(District$District), ),
-               # selecting ALL as default option
-               selected = "All",
-               multiple = TRUE
-             ),
-             selectInput(
-               "State", "Select the State Name:",
-               # Appending ALL to have a option to load all locations
-               append("All", as.list(State$State), ),
-               # selecting ALL as default option
-               selected = "All",
-               multiple = TRUE
-             ),
-             selectInput(
-               "Category", "Select the Category Name:",
-               # Appending ALL to have a option to load all locations
-               append("All", as.list(Category$Category), ),
-               # selecting ALL as default option
-               selected = "All",
-               multiple = TRUE
-             )
+      column(
+        4,
+        selectInput(
+          "District", "Select the District Name:",
+          # Appending ALL to have a option to load all locations
+          append("All", as.list(District$District), ),
+          # selecting ALL as default option
+          selected = "All",
+          multiple = TRUE
+        ),
+        selectInput(
+          "State", "Select the State Name:",
+          # Appending ALL to have a option to load all locations
+          append("All", as.list(State$State), ),
+          # selecting ALL as default option
+          selected = "All",
+          multiple = TRUE
+        ),
+        selectInput(
+          "Category", "Select the Category Name:",
+          # Appending ALL to have a option to load all locations
+          append("All", as.list(Category$Category), ),
+          # selecting ALL as default option
+          selected = "All",
+          multiple = TRUE
+        )
       )
     )
   )
@@ -106,7 +114,7 @@ logos <- awesomeIconList(
 )
 
 geosearch1 <- basicPage(
-  HTML(paste0(" <script> 
+  HTML(paste0(" <script>
                 function initAutocomplete() {
 
                 var autocomplete = new google.maps.places.Autocomplete(document.getElementById('address'),{types: ['geocode']});
@@ -139,8 +147,8 @@ geosearch1 <- basicPage(
                 Shiny.onInputChange('jsValueAddressNumber', address_number);
                 Shiny.onInputChange('jsValuePretty', addressPretty);
                 Shiny.onInputChange('jsValueCoords', coords);});}
-                </script> 
-                <script src='https://maps.googleapis.com/maps/api/js?key=", key,"&libraries=places&callback=initAutocomplete' async defer></script>"))
+                </script>
+                <script src='https://maps.googleapis.com/maps/api/js?key=", key, "&libraries=places&callback=initAutocomplete' async defer></script>"))
 )
 
 
@@ -184,17 +192,18 @@ server <- function(input, output) {
           Category %in% input$Category
         }
       )
-    
-    leaflet(filtered_data) %>%  addMapboxTiles(username = "mapbox", style_id = "streets-v11", group = "mapbox") %>%
+
+    leaflet(filtered_data) %>%
+      addMapboxTiles(username = "mapbox", style_id = "streets-v11", group = "mapbox") %>%
       addMapboxTiles(username = "mapbox", style_id = "outdoors-v11", group = "outdoors") %>%
       addMapboxTiles(username = "mapbox", style_id = "light-v10", group = "light") %>%
       addMapboxTiles(username = "mapbox", style_id = "dark-v10", group = "dark") %>%
       addMapboxTiles(username = "mapbox", style_id = "satellite-v9", group = "satellite") %>%
       setView(78.9629, 20.5937, zoom = 4) %>%
-      
-      addAwesomeMarkers(group = "Clustering", lat = ~Latitude, lng = ~Longitude,
-                        icon = ~ logos[Category],
- popup = paste0(
+      addAwesomeMarkers(
+        group = "Clustering", lat = ~Latitude, lng = ~Longitude,
+        icon = ~ logos[Category],
+        popup = paste0(
           "<p> <b>Heading: </b>", filtered_data$Heading, "</p>",
           "<img src = ", filtered_data$Image,
           ' width="100%"  height="100"', ">",
@@ -230,8 +239,8 @@ server <- function(input, output) {
           filtered_data$TalukName,
           "</p>"
         ),
-                        clusterOptions = markerClusterOptions()) %>%
-      
+        clusterOptions = markerClusterOptions()
+      ) %>%
       addAwesomeMarkers(
         group = "Markers",
         lat = ~Latitude, lng = ~Longitude,
@@ -273,22 +282,21 @@ server <- function(input, output) {
           "</p>"
         )
       ) %>%
-      
-      addHeatmap(lng = ~Longitude,
-                 lat = ~Latitude,
-                 intensity = 20,
-                 max = 100,
-                 radius = 20,
-                 blur = 20, group = "HeatMap") %>%  addSearchGoogle(searchOptions(autoCollapse = TRUE, minLength = 8)) %>%
-      
-      
+      addHeatmap(
+        lng = ~Longitude,
+        lat = ~Latitude,
+        intensity = 20,
+        max = 100,
+        radius = 20,
+        blur = 20, group = "HeatMap"
+      ) %>%
+      addSearchGoogle(searchOptions(autoCollapse = TRUE, minLength = 8)) %>%
       addLayersControl(
         position = "topright",
         baseGroups = c("mapbox", "outdoors", "light", "dark", "satellite"),
         overlayGroups = c("Clustering", "HeatMap", "geo_boundraies", "Markers"),
-        options = layersControlOptions(collapsed=TRUE)
+        options = layersControlOptions(collapsed = TRUE)
       )
-    
   })
 }
 
